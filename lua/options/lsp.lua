6bc09bdf50f38862694210b944a50d8a37ed7f53
W. Santos <w.santos.gg@gmail.com>
2023-06-24 21:25:25 +0800

    ensured installation setting

--- lua/options/lsp.lua
+++ lua/options/lsp.lua
@@ -5,6 +5,11 @@ default_opts = {
 	capabilities = capabilities
 }
 
+require('mason-lspconfig').setup {
+	ensure_installed = {'rust_analyzer', 'tsserver', 'pyright', 'clangd'},
+	automatic_installation = false
+}
+
 -- LSP
 lspconfig.clangd.setup(default_opts)
 lspconfig.pyright.setup(default_opts)
