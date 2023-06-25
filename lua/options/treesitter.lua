e58ce615854b90590a4a8054e1bb6bb2f84fea96
W. Santos <w.santos.gg@gmail.com>
2023-06-25 17:49:05 +0800

    ruby

--- lua/options/treesitter.lua
+++ lua/options/treesitter.lua
@@ -1,5 +1,5 @@
 require('nvim-treesitter.configs').setup{
-	ensure_installed = {'c', 'cpp', 'rust', 'python', 'typescript', 'vue', 'make', 'html', 'javascript'},
+	ensure_installed = {'c', 'cpp', 'rust', 'python', 'typescript', 'vue', 'make', 'html', 'javascript', 'ruby'},
 	auto_install = true,
 	highlight = {
 		enable=true
