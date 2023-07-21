a09485037220d569e57af3d84cd7f1f9f5a9c229
W. Santos <w.santos.gg@gmail.com>
2023-07-21 12:25:41 +0800

    telescope additions

--- lua/options/telescope.lua
+++ lua/options/telescope.lua
@@ -1,5 +1,12 @@
+local actions = require('telescope.actions')
+
 require('telescope').setup {
     defaults = {
+        mappings = {
+            n = {
+                ['<C-d>'] = actions.delete_buffer + actions.move_to_top,
+            }
+        },
         file_ignore_patterns={
             "%.git/", "%.git\\",
             "%.node%_modules/", "%.node%_modules\\"
