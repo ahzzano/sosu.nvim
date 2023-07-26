f56598c8bf9096148ce49c81ce6742e90623fa31
W. Santos <w.santos.gg@gmail.com>
2023-07-26 12:39:12 +0800

    statusline

--- lua/options/statusline.lua
+++ lua/options/statusline.lua
@@ -1,10 +1,16 @@
 -- LUA LINE
-local opts={
-	icons_enabled=true,
-	disabled_filetypes = { 
-		'NVimTree'
-	},
-	disabled_buftypes = {'NvimTree'}
+local opts = {
+    icons_enabled = true,
+    disabled_filetypes = {
+        'NVimTree'
+    },
+    disabled_buftypes = { 'NvimTree' },
+    options = {
+        section_separators = {
+            left='', right=''
+        },
+        component_separators = { left = '', right = '' }
+   }
 }
 
 require('lualine').setup(opts)
