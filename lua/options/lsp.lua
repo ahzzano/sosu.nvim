333fba860b93992d996adf82aae5154665e309ce
W. Santos <w.santos.gg@gmail.com>
2023-07-26 19:12:28 +0800

    cleaned up lsp

--- lua/options/lsp.lua
+++ lua/options/lsp.lua
@@ -1,25 +1,19 @@
-local lsp_config = require('lspconfig')
-local default_capabilities = require('cmp_nvim_lsp').default_capabilities()
-local get_servers = require('mason-lspconfig').get_installed_servers
-
-default_opts = {
-    capabilities = default_capabilities,
-    on_attach = function(_, _)
-        vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
-        vim.keymap.set('n', '<C-s>', vim.lsp.buf.rename, {})
-        vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
-        vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, {})
-        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-        vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-    end
-}
-
 local lsp = require('lsp-zero').preset({})
 
-lsp.on_attach(function(client, bufnr)
+lsp.on_attach(function(_, bufnr)
     lsp.default_keymaps({ buffer = bufnr })
-    lsp.buffer_autoformat()
 end)
+
+lsp.format_on_save({
+    format_opts = {
+        async = false,
+        timeout_ms = 10000
+    },
+    servers = {
+        ['gofumpt'] = { 'go' },
+        ['clang-format'] = {'c', 'cpp'},
+    }
+})
 
 lsp.set_sign_icons({
     error = '✘',
