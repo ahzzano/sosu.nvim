e4f6107640203d1b2ac95db040316f2397208517
W. Santos <w.santos.gg@gmail.com>
2023-08-27 19:39:04 +0800

    Edited the diagnostic text color

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -1,23 +1,30 @@
 if vim.bo.filetype == 'lua' then
-  require('neodev').setup()
+    require('neodev').setup()
 end
 
 local lsp = require('lsp-zero').preset()
 
-lsp.on_attach(function(_, bufnr)
-  lsp.default_keymaps({ buffer = bufnr })
-  lsp.buffer_autoformat()
-  local opts = { buffer = bufnr, remap = false }
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = " ",
+        virt_text_pos = 'right_align',
+    },
+})
 
-  vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
-  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = true })
+lsp.on_attach(function(_, bufnr)
+    lsp.default_keymaps({ buffer = bufnr })
+    lsp.buffer_autoformat()
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set('n', '<leader>vca', vim.lsp.buf.code_action, opts)
+    vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', { buffer = true })
 end)
 
 lsp.set_sign_icons({
-  error = '✘',
-  warn = '▲',
-  hint = '⚑',
-  info = '»'
+    error = '✘',
+    warn = '▲',
+    hint = '⚑',
+    info = '»'
 })
 
 lsp.skip_server_setup({ 'rust_analyzer' })
