8d0ed1bb08f415e290e0fa9c81247b8b64b77432
W. Santos <w.santos.gg@gmail.com>
2023-08-27 17:18:31 +0800

    More rust

--- lua/plugins/configs/cmp.lua
+++ lua/plugins/configs/cmp.lua
@@ -5,33 +5,34 @@ require('luasnip.loaders.from_vscode').lazy_load()
 
 local kind_icons = require('sosu.core').lspicons
 local function format(_, vim_item)
-  vim_item.kind = " " .. kind_icons[vim_item.kind] .. " " or ""
-  return vim_item
+    vim_item.kind = " " .. kind_icons[vim_item.kind] .. " " or ""
+    return vim_item
 end
 
 cmp.setup({
-  preselect = 'item',
-  completion = {
-    completeopt = 'menu,menuone,noinsert'
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  formatting = {
-    fields = { "abbr", "kind" },
-    format = format,
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'buffer' },
-  },
-  mapping = {
-    ['<C-f>'] = cmp_action.luasnip_jump_forward(),
-    ['<C-b>'] = cmp_action.luasnip_jump_backward(),
-    ['<Tab>'] = cmp_action.tab_complete(),
-    ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }),
-  },
+    preselect = 'item',
+    completion = {
+        completeopt = 'menu,menuone,noinsert'
+    },
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+        fields = { "abbr", "kind" },
+        format = format,
+    },
+    sources = {
+        { name = 'nvim_lsp' },
+        { name = 'luasnip' },
+        { name = 'buffer' },
+        { name = 'crates' }
+    },
+    mapping = {
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<Tab>'] = cmp_action.tab_complete(),
+        ['<S-Tab>'] = cmp_action.select_prev_or_fallback(),
+        ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    },
 })
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,262 +1,273 @@
 local plugins = {
-  {
-    'nvim-tree/nvim-web-devicons',
-    opts = {},
-    lazy = true
-  },
-  { 'nvim-lua/plenary.nvim' },
-
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {},
-    lazy = true,
-    event = "InsertEnter",
-  },
-
-  --#region LSP RELATED
-  -- DO NOT TOUCH
-  {
-    'neovim/nvim-lspconfig',
-    lazy = true,
-    event = { 'BufReadPre', 'BufNewFile' },
-
-    config = function(_)
-      require('plugins.configs.lspconfig')
-      require('plugins.configs.cmp')
-    end,
-  },
-  {
-    'williamboman/mason.nvim',
-    opts = {
+    {
+        'nvim-tree/nvim-web-devicons',
+        opts = {},
+        lazy = true
     },
-    build = function()
-      pcall(vim.api.nvim_command, 'MasonUpdate')
-    end,
-  },
-  {
-    'hrsh7th/nvim-cmp',
-    lazy = true,
-    event = "InsertEnter",
-    dependencies = {
-      { 'L3MON4D3/LuaSnip' },
-    }
-  },
-  {
-    'williamboman/mason-lspconfig.nvim',
-    opts = {
-      ensure_installed = {
-        'clangd',
-        'gopls',
-        'pyright',
-        'rust_analyzer',
-        'lua_ls',
-        'rome',
-        'tsserver',
-      }
-    },
-    lazy = true
-  },
-  {
-    'hrsh7th/cmp-cmdline',
-    lazy = true,
-    event = 'VeryLazy'
-  },
-  {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v2.x',
-    dependencies = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim', },
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
-      { 'hrsh7th/nvim-cmp' },                  -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },              -- Required
-      { 'L3MON4D3/LuaSnip' },                  -- Required
-      { 'saadparwaiz1/cmp_luasnip' }
-    },
-    lazy = true,
-  },
-  {
-    'L3MON4D3/LuaSnip',
-    dependencies = {
-      "rafamadriz/friendly-snippets"
-    },
-    opts = {},
-    lazy = true
-  },
-  {
-    'saadparwaiz1/cmp_luasnip',
-    lazy = true
-  },
-  {
-    'hrsh7th/cmp-nvim-lsp',
-    lazy = true
-  },
-  {
-    'mfussenegger/nvim-dap',
-    lazy = true,
-    event = "VeryLazy"
-  },
-  {
-    'rcarriga/nvim-dap-ui',
-    lazy = true,
-    event = "VeryLazy",
-    config = function()
-      require('plugins.configs.dap')
-    end
-  },
-  {
-    'jay-babu/mason-nvim-dap.nvim',
-    event = "VeryLazy",
-    lazy = true,
-    dependencies = {
-      { 'rcarriga/nvim-dap-ui' },
-      { 'mfussenegger/nvim-dap' },
-      { 'williamboman/mason-lspconfig.nvim' },
-    }
-  },
+    { 'nvim-lua/plenary.nvim' },
 
-  --#endregion
-  --#region TREESITTER
-  {
-    'nvim-treesitter/nvim-treesitter',
-    lazy = true,
-    config = function()
-      require('plugins.configs.treesitter')
-    end,
-    event = "VeryLazy",
-  },
-  {
-    'windwp/nvim-ts-autotag',
-    ft = { "html", "markdown", "xml", "typescript", "vue", "jsx" },
-    lazy = true
-  },
-  {
-    "nvim-tree/nvim-tree.lua",
-    version = "*",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
+    {
+        'lewis6991/gitsigns.nvim',
+        opts = {},
+        lazy = true,
+        event = "InsertEnter",
     },
-    keys = {
-      "<C-n>"
+
+    --#region LSP RELATED
+    -- DO NOT TOUCH
+    {
+        'neovim/nvim-lspconfig',
+        lazy = true,
+        event = { 'BufReadPre', 'BufNewFile' },
+
+        config = function(_)
+            require('plugins.configs.lspconfig')
+            require('plugins.configs.cmp')
+        end,
     },
-    config = function()
-      require('plugins.configs.nvimtree')
-    end,
-    lazy = true
-  },
-
-  --#endregion
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.1',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    lazy = true
-  },
-  {
-    'windwp/nvim-autopairs',
-    lazy = true,
-    event = "InsertEnter",
-    opts = {}
-  },
-  {
-    "catppuccin/nvim",
-    name = "catppuccin",
-    priority = 1000,
-    opts = {
-      no_italic = true,
-      term_colors = true,
-      transparent_background = false,
-      integrations = {
-        harpoon = true,
-        treesitter_context = true
-      }
-    }
-  },
-  {
-    'nvim-lualine/lualine.nvim',
-    event = "VeryLazy",
-    config = function()
-      require('plugins.configs.statusline')
-    end,
-    lazy = false,
-  },
-
-  {
-    'glepnir/dashboard-nvim',
-    event = 'VimEnter',
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
-  },
-  {
-    'simrat39/rust-tools.nvim',
-    ft = { 'rust' },
-    config = function()
-      local rust_tools = require('rust-tools')
-
-      rust_tools.setup({
-        server = {
-          on_attach = function(_, bufnr)
-            vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
-          end
+    {
+        'williamboman/mason.nvim',
+        opts = {
+        },
+        build = function()
+            pcall(vim.api.nvim_command, 'MasonUpdate')
+        end,
+    },
+    {
+        'hrsh7th/nvim-cmp',
+        lazy = true,
+        event = "InsertEnter",
+        dependencies = {
+            { 'L3MON4D3/LuaSnip' },
         }
-      })
-    end,
-    dependencies = {
-      { 'VonHeikemen/lsp-zero.nvim' },
-      { 'neovim/nvim-lspconfig' }
-    }
-  },
-  {
-    'numToStr/Comment.nvim',
-    event = "InsertEnter",
-    lazy = false,
-    config = true
-  },
-  {
-    'ThePrimeagen/harpoon',
-    lazy = true,
-    event = "VeryLazy"
-  },
-  {
-    "folke/neodev.nvim",
-    ft = { "lua" },
-    opts = {},
-    lazy = true,
-  },
-  {
-    "rebelot/kanagawa.nvim",
-    priority = 1000,
-    opts = {
-      colors = {
-        theme = {
-          all = {
-            ui = {
-              bg_gutter = "none"
+    },
+    {
+        'williamboman/mason-lspconfig.nvim',
+        opts = {
+            ensure_installed = {
+                'clangd',
+                'gopls',
+                'pyright',
+                'rust_analyzer',
+                'lua_ls',
+                'rome',
+                'tsserver',
             }
-          }
+        },
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-cmdline',
+        lazy = true,
+        event = 'VeryLazy'
+    },
+    {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v2.x',
+        dependencies = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim', },
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { 'hrsh7th/nvim-cmp' },                  -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },              -- Required
+            { 'L3MON4D3/LuaSnip' },                  -- Required
+            { 'saadparwaiz1/cmp_luasnip' }
+        },
+        lazy = true,
+    },
+    {
+        'L3MON4D3/LuaSnip',
+        dependencies = {
+            "rafamadriz/friendly-snippets"
+        },
+        opts = {},
+        lazy = true
+    },
+    {
+        'saadparwaiz1/cmp_luasnip',
+        lazy = true
+    },
+    {
+        'hrsh7th/cmp-nvim-lsp',
+        lazy = true
+    },
+    {
+        'mfussenegger/nvim-dap',
+        lazy = true,
+        event = "VeryLazy"
+    },
+    {
+        'rcarriga/nvim-dap-ui',
+        lazy = true,
+        event = "VeryLazy",
+        config = function()
+            require('plugins.configs.dap')
+        end
+    },
+    {
+        'jay-babu/mason-nvim-dap.nvim',
+        event = "VeryLazy",
+        lazy = true,
+        dependencies = {
+            { 'rcarriga/nvim-dap-ui' },
+            { 'mfussenegger/nvim-dap' },
+            { 'williamboman/mason-lspconfig.nvim' },
         }
-      }
-    }
-  },
-  {
-    'nvim-treesitter/nvim-treesitter-context',
-    opts = {},
-    dependencies = {
-      { 'nvim-treesitter/nvim-treesitter' }
     },
-    lazy = true,
-    event = "InsertEnter"
-  },
-  {
-    "NeogitOrg/neogit",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim",
+
+    --#endregion
+    --#region TREESITTER
+    {
+        'nvim-treesitter/nvim-treesitter',
+        lazy = true,
+        config = function()
+            require('plugins.configs.treesitter')
+        end,
+        event = "VeryLazy",
     },
-    config = true,
-    event = "VeryLazy",
-    lazy = true
-  },
+    {
+        'windwp/nvim-ts-autotag',
+        ft = { "html", "markdown", "xml", "typescript", "vue", "jsx" },
+        lazy = true
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        keys = {
+            "<C-n>"
+        },
+        config = function()
+            require('plugins.configs.nvimtree')
+        end,
+        lazy = true
+    },
+
+    --#endregion
+    {
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.1',
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        lazy = true
+    },
+    {
+        'windwp/nvim-autopairs',
+        lazy = true,
+        event = "InsertEnter",
+        opts = {}
+    },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        priority = 1000,
+        opts = {
+            no_italic = true,
+            term_colors = true,
+            transparent_background = false,
+            integrations = {
+                harpoon = true,
+                treesitter_context = true
+            }
+        }
+    },
+    {
+        'nvim-lualine/lualine.nvim',
+        event = "VeryLazy",
+        config = function()
+            require('plugins.configs.statusline')
+        end,
+        lazy = false,
+    },
+
+    {
+        'glepnir/dashboard-nvim',
+        event = 'VimEnter',
+        dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+    },
+    {
+        'simrat39/rust-tools.nvim',
+        ft = { 'rust' },
+        config = function()
+            local rust_tools = require('rust-tools')
+
+            rust_tools.setup({
+                server = {
+                    on_attach = function(_, bufnr)
+                        vim.keymap.set('n', '<leader>ca', rust_tools.hover_actions.hover_actions, { buffer = bufnr })
+                    end
+                }
+            })
+        end,
+        dependencies = {
+            { 'VonHeikemen/lsp-zero.nvim' },
+            { 'neovim/nvim-lspconfig' }
+        }
+    },
+    {
+        'saecki/crates.nvim',
+        ft = { 'rust', 'toml' },
+        lazy = true,
+        config = function(_, opts)
+            local crates = require('crates')
+            crates.setup(opts)
+            crates.show()
+        end
+
+    },
+    {
+        'numToStr/Comment.nvim',
+        event = "InsertEnter",
+        lazy = false,
+        config = true
+    },
+    {
+        'ThePrimeagen/harpoon',
+        lazy = true,
+        event = "VeryLazy"
+    },
+    {
+        "folke/neodev.nvim",
+        ft = { "lua" },
+        opts = {},
+        lazy = true,
+    },
+    {
+        "rebelot/kanagawa.nvim",
+        priority = 1000,
+        opts = {
+            colors = {
+                theme = {
+                    all = {
+                        ui = {
+                            bg_gutter = "none"
+                        }
+                    }
+                }
+            }
+        }
+    },
+    {
+        'nvim-treesitter/nvim-treesitter-context',
+        opts = {},
+        dependencies = {
+            { 'nvim-treesitter/nvim-treesitter' }
+        },
+        lazy = true,
+        event = "InsertEnter"
+    },
+    {
+        "NeogitOrg/neogit",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "nvim-telescope/telescope.nvim",
+        },
+        config = true,
+        event = "VeryLazy",
+        lazy = true
+    },
 
 }
 
