671eafa6330e8175441b07033d1cf1665f15fb9d
ahzzan <w.santos.gg@gmail.com>
2023-09-06 18:11:05 +0800

    colors are weird

--- lua/sosu/colors.lua
+++ lua/sosu/colors.lua
@@ -5,20 +5,20 @@ function SetColorscheme(color)
     -- local hl = vim.api.nvim_get_hl_by_name('Normal')
     -- print(hl)
 
-    local hl = vim.api.nvim_get_hl(0, {
-        name = "Normal"
-    })
-    local bg = hl.bg
-
-    local vt_err = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextError" }).fg
-    local vt_wrn = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextWarn" }).fg
-    local vt_inf = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextInfo" }).fg
-    local vt_hin = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextHint" }).fg
-
-    vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { bg = bg, fg = vt_err })
-    vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { bg = bg, fg = vt_wrn })
-    vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { bg = bg, fg = vt_inf })
-    vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { bg = bg, fg = vt_hin })
+    -- local hl = vim.api.nvim_get_hl(0, {
+    --     name = "Normal"
+    -- })
+    -- local bg = hl.bg
+    --
+    -- local vt_err = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextError" }).fg
+    -- local vt_wrn = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextWarn" }).fg
+    -- local vt_inf = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextInfo" }).fg
+    -- local vt_hin = vim.api.nvim_get_hl(0, { name = "DiagnosticVirtualTextHint" }).fg
+    --
+    -- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextError", { bg = bg, fg = vt_err })
+    -- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextWarn", { bg = bg, fg = vt_wrn })
+    -- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextInfo", { bg = bg, fg = vt_inf })
+    -- vim.api.nvim_set_hl(0, "DiagnosticVirtualTextHint", { bg = bg, fg = vt_hin })
 end
 
 vim.opt.background = "dark"
