07bcdb135416daa7d4aac4337a3aa3ed7a3dfd41
W. Santos <w.santos.gg@gmail.com>
2024-01-26 10:59:15 +0800

    no more clang-format

--- lua/plugins/configs/null-ls.lua
+++ lua/plugins/configs/null-ls.lua
@@ -4,10 +4,8 @@ null_ls.setup({
     sources = {
         null_ls.builtins.code_actions.gitsigns,
         null_ls.builtins.formatting.rustfmt,
-        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.gofmt,
         null_ls.builtins.formatting.goimports,
-        -- null_ls.builtins.completion.spell,
     }
 })
