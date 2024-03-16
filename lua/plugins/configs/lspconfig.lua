2ed5a782dc2ff0ff5727572aaa3cb952a0710ead
W. Santos <w.santos.gg@gmail.com>
2024-03-16 10:26:18 +0800

    goodbye null-ls

--- lua/plugins/configs/lspconfig.lua
+++ lua/plugins/configs/lspconfig.lua
@@ -35,10 +35,6 @@ lsp.on_attach(function(_, bufnr)
 	lsp.default_keymaps({ buffer = bufnr })
 	lsp.buffer_autoformat()
 	local opts = { buffer = bufnr, remap = false }
-
-	vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
-	vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", { buffer = true })
-	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 end)
 
 lsp.set_sign_icons({
