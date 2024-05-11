57c401cdf691a3c575a12bac84b69d22814d438f
W. Santos <w.santos.gg@gmail.com>
2024-05-12 00:24:30 +0800

    more neogit events

--- lua/plugins/configs/neogit.lua
+++ lua/plugins/configs/neogit.lua
@@ -13,3 +13,12 @@ end)
 vim.keymap.set('n', '<leader>ngc', function()
     neogit.open({ "commit", kind = 'vsplit' })
 end)
+
+-- useful events
+local group = vim.api.nvim_create_augroup('MyNeogitEvents', { clear = true })
+
+vim.api.nvim_create_autocmd('User', {
+    pattern = 'NeogitPushComplete',
+    group = group,
+    callback = neogit.close,
+})
