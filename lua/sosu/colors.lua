d65c5725bc843d70705db62d098206924d5748d3
ahzzano <w.santos.gg@gmail.com>
2024-07-01 09:08:28 +0000

    transparency

--- lua/sosu/colors.lua
+++ lua/sosu/colors.lua
@@ -2,15 +2,15 @@ function SetColorscheme(color)
     color = color or "catppuccin"
 
     require('catppuccin').setup({
-        -- transparent_background = true,
+        transparent_background = true,
         highlight_overrides = {
             mocha = function(mocha)
                 return {
-                    TelescopeTitle = { bg = mocha.green, fg = mocha.base },
-                    TelescopeNormal = { bg = mocha.mantle },
-                    -- TelescopePromptNormal = { bg = mocha.crust },
-                    -- TelescopePromptBorder = { bg = mocha.crust, fg = mocha.crust },
-                    TelescopeBorder = { bg = mocha.mantle, fg = mocha.mantle }
+                    -- TelescopeTitle = { bg = mocha.green, fg = mocha.base },
+                    -- TelescopeNormal = { bg = mocha.mantle },
+                    -- -- TelescopePromptNormal = { bg = mocha.crust },
+                    -- -- TelescopePromptBorder = { bg = mocha.crust, fg = mocha.crust },
+                    -- TelescopeBorder = { bg = mocha.mantle, fg = mocha.mantle }
                 }
             end
         }
