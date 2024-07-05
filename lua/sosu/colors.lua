b406cbdec63c345fc5753c02264bce98ecabde2b
W. Santos <w.santos.gg@gmail.com>
2024-07-05 10:04:01 +0800

    tokyonight

--- lua/sosu/colors.lua
+++ lua/sosu/colors.lua
@@ -1,20 +1,20 @@
 function SetColorscheme(color)
-    color = color or "catppuccin"
+    color = color or "tokyonight-storm"
 
-    require('catppuccin').setup({
-        transparent_background = true,
-        highlight_overrides = {
-            mocha = function(mocha)
-                return {
-                    -- TelescopeTitle = { bg = mocha.green, fg = mocha.base },
-                    -- TelescopeNormal = { bg = mocha.mantle },
-                    -- -- TelescopePromptNormal = { bg = mocha.crust },
-                    -- -- TelescopePromptBorder = { bg = mocha.crust, fg = mocha.crust },
-                    -- TelescopeBorder = { bg = mocha.mantle, fg = mocha.mantle }
-                }
-            end
-        }
-    })
+    -- require('catppuccin').setup({
+    --     transparent_background = true,
+    --     highlight_overrides = {
+    --         mocha = function(mocha)
+    --             return {
+    --                 -- TelescopeTitle = { bg = mocha.green, fg = mocha.base },
+    --                 -- TelescopeNormal = { bg = mocha.mantle },
+    --                 -- -- TelescopePromptNormal = { bg = mocha.crust },
+    --                 -- -- TelescopePromptBorder = { bg = mocha.crust, fg = mocha.crust },
+    --                 -- TelescopeBorder = { bg = mocha.mantle, fg = mocha.mantle }
+    --             }
+    --         end
+    --     }
+    -- })
     vim.cmd.colorscheme(color)
 end
 
