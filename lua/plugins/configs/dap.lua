6130cfffa3fee93245efb4730d70307123b75a85
W. Santos <w.santos.gg@gmail.com>
2025-01-19 15:04:16 +0800

    rust debug

--- lua/plugins/configs/dap.lua
+++ lua/plugins/configs/dap.lua
@@ -37,6 +37,20 @@ dap.configurations.cpp = {
     },
 }
 
+dap.configurations.rust = {
+    {
+        name = 'Launch',
+        type = 'codelldb',
+        request = 'launch',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtBeginningOfMainSubprogram = false,
+        stopOnEntry = false,
+    },
+}
+
 dap.configurations.c = dap.configurations.cpp
 
 
