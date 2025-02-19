17be2c2f64190dd7d393e18bfd66c0dfa6f5c88e
William Santos <w.santos.gg@gmail.com>
2025-02-19 13:15:31 +0800

    Init time

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -265,6 +265,8 @@ local plugins = {
         "NeogitOrg/neogit",
         dependencies = {
             "nvim-lua/plenary.nvim",
+            "sindrets/diffview.nvim",
+            "nvim-telescope/telescope.nvim",
         },
         config = function()
             require("plugins.configs.neogit")
