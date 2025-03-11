d3624f2431e8458b7907728706cc4dae0d89eead
William Santos <w.santos.gg@gmail.com>
2025-03-11 09:07:16 +0800

    meow

--- lua/plugins/configs/lualine.lua
+++ lua/plugins/configs/lualine.lua
@@ -18,8 +18,8 @@ local opts = {
             }
         },
         section_separators = {
-            left = '',
-            right = ''
+            -- left = '',
+            -- right = ''
         },
         component_separators = { left = '|', right = '' },
     },
